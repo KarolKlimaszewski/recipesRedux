ea50a64812f371d7b058561e62bd24f151b96afe
Karol Klimaszewski <karol.klimaszewski1997@gmail.com>
2018-10-31 14:58:15 +0100

    starting point

---
+++ src/js/reducers/index.js
@@ -0,0 +1,19 @@
+import { ADD_ARTICLE } from "../constants/action-types";
+import { DELETE_ARTICLE } from "../constants/action-types";
+const initialState = {
+  articles: []
+};
+const rootReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_ARTICLE:
+      return {state, articles: [...state.articles, action.payload] };
+    case DELETE_ARTICLE:
+      return {
+        state,
+        articles: [...state.articles.slice(0, action.payload), ...state.articles.slice(action.payload + 1)]
+      };
+    default:
+      return state;
+  }
+};
+export default rootReducer;
